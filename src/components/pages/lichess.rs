ba2d4dba027486cd9a3017f53391e64cea1fd912
Storm <stormpower44@protonmail.com>
2023-12-08 19:45:10 -0700

    add lichess json GET request

--- src/components/pages/lichess.rs
+++ src/components/pages/lichess.rs
@@ -1,9 +1,19 @@
-use yew::prelude::*;
 use crate::components::molecules::username_input::UsernameInput;
-use reqwest::{Client, StatusCode};
 use gloo::console::log;
+use reqwest::{Client, StatusCode};
+use serde::Deserialize;
+use yew::prelude::*;
 
 use wasm_bindgen_futures;
+
+#[derive(Debug, Deserialize)]
+struct TimeControlStats {
+    games: u32,
+    rating: u32,
+    rd: u32,
+    prog: u32,
+    prov: bool,
+}
 
 #[function_component(Lichess)]
 pub fn lichess() -> Html {
@@ -11,20 +21,37 @@ pub fn lichess() -> Html {
         wasm_bindgen_futures::spawn_local(async move {
             let client = Client::new();
 
-            let username_response = client
-                .post(format!("https://lichess.org/api/user/{}", username))
+            let username_response: serde_json::Value = client
+                .get(format!("https://lichess.org/api/user/{}", username))
+                .header("Content-Type", "application/json")
                 .send()
-                .await;
+                .await
+                .unwrap()
+                .json()
+                .await
+                .unwrap();
 
-            match username_response {
-                Ok(username_response) => {
-                    if username_response.status() == StatusCode::CREATED {
-                        log!("Got user data.");
-                    }
+            log!(format!("JSON Response: {:?}", username_response));
+
+            if let (Some(user_id), Some(username), Some(perfs)) = (
+                username_response.get("id"),
+                username_response.get("username"),
+                username_response.get("perfs"),
+            ) {
+                log!(format!("GOT USERNAME: {}", username));
+                log!(format!("GOT ID: {}", user_id));
+
+                if let (Some(bullet), Some(blitz), Some(rapid)) =
+                    (perfs.get("bullet"), perfs.get("blitz"), perfs.get("rapid"))
+                {
+                    let bullet: TimeControlStats = serde_json::from_value(bullet.clone()).unwrap();
+                    let blitz: TimeControlStats = serde_json::from_value(blitz.clone()).unwrap();
+                    let rapid: TimeControlStats = serde_json::from_value(rapid.clone()).unwrap();
+
+                    log!(format!("{:?}", bullet.rating));
                 }
-                Err(err) => {
-                    println!("HTTP request Error: {:?}", err);
-                }
+            } else {
+                log!("User most likely does not exist.")
             }
         });
     });
@@ -35,3 +62,4 @@ pub fn lichess() -> Html {
         </main>
     }
 }
+
